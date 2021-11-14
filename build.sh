04b68879489268b2a08dea59218e47587c1df147
Dimitri Belopopsky <dimitri@belopopsky.com>
2021-11-14 23:56:25 +0100

    Update build system

--- build.sh
+++ build.sh
@@ -14,15 +14,16 @@ fi
 COMPILER=g++
 
 STANDARD=-std=c++20
+COMMMON=(-faligned-new)
 WARNINGS=(-Wall -Wextra -Wdisabled-optimization -Wno-unknown-pragmas -Wno-deprecated-copy -Wshadow=compatible-local -Wnon-virtual-dtor -Wold-style-cast -Wcast-align -Wunused -Woverloaded-virtual -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wformat=2)
 SANITIZERS=(-fsanitize="address,leak,undefined" -fsanitize-address-use-after-scope)
 DEBUG=(-Og -g3 -ggdb -fstack-protector -ftrapv -fno-omit-frame-pointer)
 OTHER=(-fopenmp)
-OPTIMISE=(-foptimize-sibling-calls -faligned-new)
+OPTIMISE=(-foptimize-sibling-calls)
 RELEASE=(-O3)
 
 if [ $MODE = "debug" ]; then
-    $COMPILER -Wl,-O1 $STANDARD "${DEBUG[@]}" "${WARNINGS[@]}"  "${SANITIZERS[@]}" "${OTHER[@]}" src/main.cpp ${OUTPUT:+ -o "$OUTPUT"}
+    $COMPILER -Wl,-O1 $STANDARD "${COMMMON[@]}" "${DEBUG[@]}" "${WARNINGS[@]}" "${SANITIZERS[@]}" "${OTHER[@]}" src/main.cpp ${OUTPUT:+ -o "$OUTPUT"}
 elif [ "${MODE,,}" = "release" ]; then
-    $COMPILER -Wl,-O1 $STANDARD "${RELEASE[@]}" "${OPTIMISE[@]}" "${WARNINGS[@]}" "${OTHER[@]}" src/main.cpp ${OUTPUT:+ -o "$OUTPUT"}
+    $COMPILER -Wl,-O1 $STANDARD "${COMMMON[@]}" "${RELEASE[@]}" "${OPTIMISE[@]}" "${WARNINGS[@]}" "${OTHER[@]}" src/main.cpp ${OUTPUT:+ -o "$OUTPUT"}
 fi
